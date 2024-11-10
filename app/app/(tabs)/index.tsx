b35716b20e1d715c09f112169b6a1357f7e38b00
Andrew Ho <87453706+anmho@users.noreply.github.com>
2024-11-10 13:38:15 -0800

    use polyfills

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -4,6 +4,9 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { polyfills } from "@/app/polyfill.native";
+
+polyfills();
 
 export default function HomeScreen() {
   return (
