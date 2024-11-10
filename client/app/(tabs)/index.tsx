957c6dd72acfe7f2755275179e3c116e182109d3
Joshua Goon <j.j.jappiecraft@gmail.com>
2024-11-09 21:44:06 -0800

    move text into prior import

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,11 +1,9 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { Image, StyleSheet, Platform, Text } from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-
-import { Text } from "react-native";
 
 export default function HomeScreen() {
   return (
