96428f3782c99a8630752e7b90e09e60bf72c0fb
Andrew Ho <87453706+anmho@users.noreply.github.com>
2024-11-15 20:27:35 -0800

    clean up server startup code

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -38,15 +38,7 @@ type Options struct {
 	Debug bool   `doc:"Enable debug logging"`
 	Host  string `doc:"Hostname to listen on."`
 	Port  int    `doc:"Port to listen on." short:"p" default:"8888"`
-}
-
-func pgConnString(config Config) string {
-	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s",
-		config.DbHost,
-		config.DbPort,
-		config.DbUser,
-		config.DbPass,
-		config.DbName)
+	Stage string `doc:"environment" short:"s" default:"development"`
 }
 
 var api huma.API
@@ -59,19 +51,22 @@ func main() {
 		var srv http.Server
 
 		hooks.OnStart(func() {
-			// Generate openapi.yaml after the server starts
-
-			// Load .env
-			err := godotenv.Load(".env")
-			if err != nil {
-				return
+			var err error
+			if opts.Stage == "development" {
+				// Load .env
+				err := godotenv.Load(".env")
+				if err != nil {
+					slog.Error("loading env", slog.Any("error", err))
+					os.Exit(1)
+				}
 			}
 
 			// Parse env into config
 			var config Config
 			err = env.Parse(&config)
 			if err != nil {
-				return
+				slog.Error("parsing env to config", slog.Any("error", err))
+				os.Exit(1)
 			}
 
 			logger := slog.Default()
@@ -82,15 +77,18 @@ func main() {
 			// Postgres
 			db, err := sql.Open("pgx", connString)
 			if err != nil {
-				panic(err)
+				slog.Error("opening database", slog.Any("error", err))
+				os.Exit(1)
 			}
 			if err := db.Ping(); err != nil {
-				panic(err)
+				slog.Error("pinging db", slog.Any("error", err))
+				os.Exit(1)
 			}
 
 			cfg, err := awsConfig.LoadDefaultConfig(ctx)
 			if err != nil {
-				log.Fatalln(err)
+				slog.Error("loading default aws config", slog.Any("error", err))
+				os.Exit(1)
 			}
 
 			// Setup S3 bucket
@@ -103,6 +101,7 @@ func main() {
 				Addr:    fmt.Sprintf(":%d", Port),
 				Handler: api.Adapter(),
 			}
+
 			logger.Info("server listening", slog.Int("port", Port))
 			if err = srv.ListenAndServe(); err != nil {
 				if errors.Is(err, http.ErrServerClosed) {
@@ -113,7 +112,6 @@ func main() {
 					os.Exit(1)
 				}
 			}
-
 		})
 
 		hooks.OnStop(func() {
@@ -140,4 +138,13 @@ func main() {
 	})
 
 	cli.Run()
+}
+
+func pgConnString(config Config) string {
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s",
+		config.DbHost,
+		config.DbPort,
+		config.DbUser,
+		config.DbPass,
+		config.DbName)
 }
