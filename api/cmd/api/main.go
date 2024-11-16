0b4a377003b4adab87804e535de681a25f67fb8c
Andrew Ho <87453706+anmho@users.noreply.github.com>
2024-11-15 20:30:42 -0800

    change server startup default port

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -37,7 +37,7 @@ const (
 type Options struct {
 	Debug bool   `doc:"Enable debug logging"`
 	Host  string `doc:"Hostname to listen on."`
-	Port  int    `doc:"Port to listen on." short:"p" default:"8888"`
+	Port  int    `doc:"Port to listen on." short:"p" default:"8080"`
 	Stage string `doc:"environment" short:"s" default:"development"`
 }
 
