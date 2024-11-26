2a6a535b53d0a07b02b5fb7e2ba5a4af3d452654
Andrew Ho <87453706+anmho@users.noreply.github.com>
2024-11-26 02:14:23 -0800

    deletion script

---
+++ infra/helm/delete.sh
@@ -0,0 +1,7 @@
+
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+# Sanitize BRANCH_NAME
+BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/[^a-zA-Z0-9-]/-/g')
+
+NAME="happened-$BRANCH_NAME"
+gcloud run services delete happened "$NAME"
