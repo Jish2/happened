042facd34a9b500e69df8919461e7027f64fbd02
Andrew Ho <87453706+anmho@users.noreply.github.com>
2024-11-26 02:14:23 -0800

    deletion script

--- infra/helm/deploy.sh
+++ infra/helm/deploy.sh
@@ -5,5 +5,6 @@ BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 # Sanitize BRANCH_NAME
 BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/[^a-zA-Z0-9-]/-/g')
 
+NAME="happened-$BRANCH_NAME"
 gcloud run services replace service.yaml --quiet
-gcloud run services set-iam-policy "happened-$BRANCH_NAME" policy.yaml --region us-west1 --quiet
+gcloud run services set-iam-policy "$NAME" policy.yaml --region us-west1 --quiet
