d5defca47d8a3cf9c1ac12d2f3cefa9ccad9131e
Andrew Ho <87453706+anmho@users.noreply.github.com>
2024-11-26 01:45:36 -0800

    fix deploy.sh
    specigy region us-west1
    
    remove unused

--- infra/helm/deploy.sh
+++ infra/helm/deploy.sh
@@ -5,6 +5,5 @@ BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 # Sanitize BRANCH_NAME
 BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/[^a-zA-Z0-9-]/-/g')
 
-SERVICE="happened-$BRANCH_NAME"
-gcloud run services replace service.yaml
-gcloud run services set-iam-policy $SERVICE policy.yaml
+gcloud run services replace service.yaml --quiet
+gcloud run services set-iam-policy "happened-$BRANCH_NAME" policy.yaml --region us-west1 --quiet
